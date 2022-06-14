0466fafaf316f69904f2a81119bdb66aa7424d0c
nerya123 <106067326+nerya123@users.noreply.github.com>
2022-06-14 20:54:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+
+while True:
+    try:
+        userinput = int(input("Enter number(1-stone, 2-scissors, 3-paper) :"))
+    except ValueError:
+        print("you entered invalid input")
+        continue
+    if userinput not in range(1, 4):
+        print("you entered invalid integer")
+        continue
+    print("user choise=" + str(userinput))
+    usernum = userinput
+
+    compnum = random.randint(1, 3)
+    print("computer random choise=" + str(compnum))
+    if usernum == compnum:
+        print("its a tie play agein")
+
+    if (usernum == 1 and compnum == 2) or (usernum == 2 and compnum == 3) or (usernum == 3 and compnum == 1):
+        print("user win!!")
+
+    if (usernum == 1 and compnum == 3) or (usernum == 2 and compnum == 1) or (usernum == 3 and compnum == 2):
+        print("computer win!!")
+    game = input("do you want to play again? (yes = y , no = n)")
+    yes = "y"
+    no = "n"
+    game != yes and game != no;
+    if game == no:
+        print("game over")
+        break
+        if game == yes:
+            print("game on!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
